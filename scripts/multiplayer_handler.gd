eb1ad864670aeacfdc3366ef2589b5482c45ab81
Username4D <studio@kremzow.org>
2025-11-28 15:22:26 +0100

    added multiplayer handler

--- scripts/multiplayer_handler.gd
+++ scripts/multiplayer_handler.gd
@@ -1,1 +1,10 @@
 extends Node
+
+const IP_ADDRESS: String = "localhost"
+const PORT: int = 56983
+
+var peer: ENetMultiplayerPeer
+func create_player():
+	peer = ENetMultiplayerPeer.new()
+	peer.create_client(IP_ADDRESS, PORT)
+	multiplayer.multiplayer_peer = peer
